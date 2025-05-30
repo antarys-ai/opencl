c480f2cd06484630f2d1f0d6d4ad0094234237bf
Adib Mohsin <adibmohsin.root@gmail.com>
2025-05-30 15:15:56 +0600

    linux will include cl_ext and cl_gl

--- cl/cl.h
+++ cl/cl.h
@@ -1,8 +1,23 @@
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+#define CL_TARGET_OPENCL_VERSION 120
+
 #if defined(__APPLE__)
-#   include <OpenCL/cl.h>
-#   include <OpenCL/cl_ext.h>
+    #include <OpenCL/cl.h>
+    #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#elif defined(_WIN32) || defined(_WIN64)
+    #include <CL/cl.h>
+    #include <CL/cl_ext.h>
+    #ifdef __has_include
+        #if __has_include(<CL/cl_gl.h>)
+            #include <CL/cl_gl.h>
+        #endif
+    #endif
 #else
-#   include <CL/cl.h>
-#   include <CL/cl_ext.h>
+    #include <CL/cl.h>
+    #include <CL/cl_ext.h>
+    #ifdef __has_include
+        #if __has_include(<CL/cl_gl.h>)
+            #include <CL/cl_gl.h>
+        #endif
+    #endif
 #endif
